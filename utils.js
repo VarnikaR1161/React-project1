84cec53bcd89adb0efd0040054da57f35f546b99
VarnikaR1161 <varnikaramesh0354@gmail.com>
2025-04-13 19:55:43 +0530

    Add files via upload

--- utils.js
+++ utils.js
@@ -1,74 +1,14 @@
-/**
- * Debounce a function to delay invoking until wait (ms) have elapsed since the last invocation.
- * @param {function(...*): *} fn The function to be debounced.
- * @param {number} wait Milliseconds to wait before invoking again.
- * @return {function(...*): void} The debounced function.
- */
-function debounce(fn, wait) {
-  /**
-   * A cached setTimeout handler.
-   * @type {number | undefined}
-   */
-  let timer;
+function stringify (obj, { EOL = '\n', finalEOL = true, replacer = null, spaces } = {}) {
+  const EOF = finalEOL ? EOL : ''
+  const str = JSON.stringify(obj, replacer, spaces)
 
-  /**
-   * @returns {void}
-   */
-  function debounced() {
-    const context = this;
-    const args = arguments;
-
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      return fn.apply(context, args);
-    }, wait);
-  }
-
-  return debounced;
+  return str.replace(/\n/g, EOL) + EOF
 }
 
-/**
- * Prettify a filename from error stacks into the desired format.
- * @param {string} filename The filename to be formatted.
- * @returns {string} The formatted filename.
- */
-function formatFilename(filename) {
-  // Strip away protocol and domain for compiled files
-  const htmlMatch = /^https?:\/\/(.*)\/(.*)/.exec(filename);
-  if (htmlMatch && htmlMatch[1] && htmlMatch[2]) {
-    return htmlMatch[2];
-  }
-
-  // Strip everything before the first directory for source files
-  const sourceMatch = /\/.*?([^./]+[/|\\].*)$/.exec(filename);
-  if (sourceMatch && sourceMatch[1]) {
-    return sourceMatch[1].replace(/\?$/, '');
-  }
-
-  // Unknown filename type, use it as is
-  return filename;
+function stripBom (content) {
+  // we do this because JSON.parse would convert it to a utf8 string if encoding wasn't specified
+  if (Buffer.isBuffer(content)) content = content.toString('utf8')
+  return content.replace(/^\uFEFF/, '')
 }
 
-/**
- * Remove all children of an element.
- * @param {HTMLElement} element A valid HTML element.
- * @param {number} [skip] Number of elements to skip removing.
- * @returns {void}
- */
-function removeAllChildren(element, skip) {
-  /** @type {Node[]} */
-  const childList = Array.prototype.slice.call(
-    element.childNodes,
-    typeof skip !== 'undefined' ? skip : 0
-  );
-
-  for (let i = 0; i < childList.length; i += 1) {
-    element.removeChild(childList[i]);
-  }
-}
-
-module.exports = {
-  debounce: debounce,
-  formatFilename: formatFilename,
-  removeAllChildren: removeAllChildren,
-};
+module.exports = { stringify, stripBom }
